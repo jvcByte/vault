16c057f3866d00066c2493b3de7c2618b538c457
jvcbyte <jvc8463@gmail.com>
2025-09-24 16:21:30 +0100

    pass tkn parameters

--- src/ERC20.sol
+++ src/ERC20.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import {ERC20} from "@openzeppelin/token/ERC20/ERC20.sol";
 
 contract ERC20Mock is ERC20 {
-    constructor() ERC20("VaultToken", "VT") {}
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
 
     function mint(address to, uint256 amount) external {
         _mint(to, amount);
